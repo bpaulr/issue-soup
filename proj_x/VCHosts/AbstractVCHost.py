e56a151a622750c53a533975dc17f1230368ec14
bradleypaulread <bradleypaulread@gmail.com>
2020-06-07 15:43:54 +0100

    Implemented abstract base class for VCHosts

---
+++ proj_x/VCHosts/AbstractVCHost.py
@@ -0,0 +1,19 @@
+from abc import ABC
+from abc import abstractmethod
+
+
+class AbstractVCHost(ABC):
+
+    def __init__(self, config, api_key=None) -> None:
+        self.config = config
+        if api_key is None:
+            # TODO: check for env var here and throw exception if its not there
+            pass
+        super().__init__()
+
+    @abstractmethod
+    def new_issue(self, tag: str, content: str) -> int:
+        pass
+
+    def __repr__(self):
+        return f"{type(self)}(config={self.config})"
