c2a29b55f5ca674aa2f6f4be44175c0d45d9d827
bradleypaulread <bradleypaulread@gmail.com>
2020-06-07 15:44:55 +0100

    Implemented proof of concept VCHost class for GitLab

---
+++ proj_x/VCHosts/VCHostGitLab.py
@@ -0,0 +1,25 @@
+from proj_x.VCHosts.AbstractVCHost import AbstractVCHost
+
+from proj_x.util import create_query
+import requests
+from os import getenv
+
+API_URL = "https://gitlab.com/api/v4/"
+ISSUE_URL = API_URL + "projects/{}/issues"
+
+
+class VCHostGitLab(AbstractVCHost):
+
+    def new_issue(self, tag: str, content: str) -> int:
+        project_url = self.__get_project_url()
+        query_args = {
+            "title": "This is a todo",
+            "label": tag,
+            "description": content,
+        }
+        issue_query = create_query(query_args)
+        response = requests.post(project_url + issue_query, headers={"PRIVATE-TOKEN": getenv("API_KEY")})
+        return response.json()["iid"]
+
+    def __get_project_url(self) -> str:
+        return ISSUE_URL.format(self.config["PROJECT_ID"])
